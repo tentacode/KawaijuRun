b6788cd61da7daaefb2655c9dd08b4401303fa3a
leolefebvre <leolefebvre.contact@gmail.com>
2015-11-14 10:44:39 +0100

    Add default camera behavior

---
+++ Assets/Scripts/Camera/PlayerFollower.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerFollower : MonoBehaviour
+{
+    public GameObject player; // reference to the player
+    public Vector2 defaultPosition;
+
+    void FixedUpdate()
+    {
+        transform.position = new Vector3(player.transform.position.x + defaultPosition.x,
+                                         transform.position.y,
+                                         transform.position.z);
+    }
+}
