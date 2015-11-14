6fedf8c9f5bda9bce53eba0e754a711671d3604c
leolefebvre <leolefebvre.contact@gmail.com>
2015-11-14 18:25:12 +0100

    Begin work on helico

---
+++ Assets/Scripts/ennemies/HelicoAmmo.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HelicoAmmo : Ammo
+{
+
+    public float speed;
+
+    public override void Launch()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(-1f * speed, 0);
+    }
+
+    void FixedUpdate()
+    {
+
+    }
+}
