55aa23027438a36f677982e0cda9a3114492a2fa
tentacode <contact@gabrielpillet.com>
2015-11-14 17:15:03 +0100

    Refacto scenarios

--- Assets/Scripts/ennemies/EnnemyController.cs
+++ Assets/Scripts/ennemies/EnnemyController.cs
@@ -1,27 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 using System;
+using System.Linq;
+
+[Serializable]
+public struct ScenarioStep
+{
+    public enum EnnemyActions { wait, shoot, move };
+
+    public EnnemyActions action;
+    public float delayForNextStep;
+}
 
 public class EnnemyController : MonoBehaviour
 {
-    public EnnemyScenario scenario;
+    public List<ScenarioStep> scenario;
     public GameObject shootPoint;
     public GameObject ammo;
 
     public float movingSpeed;
 
-    private bool isMoving = false;
     private Rigidbody2D rb;
-    
+    private int stepIndex = 0;
+    private bool isTriggered = false;
 
     // Use this for initialization
-    void Start () {
+    void Start ()
+    {
         rb = GetComponent<Rigidbody2D>();
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
 	}
 
     void OnTriggerEnter2D(Collider2D other)
@@ -38,49 +45,63 @@ public class EnnemyController : MonoBehaviour
         }
     }
 
-    public void setState(ScenarioStep.EnnemyActions action)
+    void BeginBehavior()
     {
-        Debug.Log("set action to " + action);
-        switch (action)
+        if (isTriggered) {
+            return;
+        }
+
+        isTriggered = true;
+        Debug.Log("Begin behavior");
+        StartCoroutine(ExecuteStep());
+    }
+
+    IEnumerator ExecuteStep()
+    {
+        ScenarioStep step = scenario[stepIndex];
+
+        Debug.Log("set action to " + step.action);
+        switch (step.action)
         {
             case ScenarioStep.EnnemyActions.move:
-                move();
+                Move();
                 break;
 
             case ScenarioStep.EnnemyActions.shoot:
-                shoot();
+                Shoot();
                 break;
 
             case ScenarioStep.EnnemyActions.wait:
-                stop();
-                break;  
+                Stop();
+                break;
         }
+
+        yield return new WaitForSeconds(step.delayForNextStep);
+        stepIndex++;
+        if (stepIndex >= scenario.Count) {
+            stepIndex = 0;
+        }
+
+        StartCoroutine(ExecuteStep());
     }
 
-    void BeginBehavior()
-    {
-        scenario.beginScenario(setState);
-    }
-
-    void shoot()
+    void Shoot()
     {
         Debug.Log("shoot");
         Instantiate(ammo, gameObject.transform.position, Quaternion.identity);
         ammo.GetComponent<Ammo>().Launch();
     }
 
-    void move()
+    void Move()
     {
         Debug.Log("move");
         rb.velocity = new Vector2(-1f * movingSpeed, 0f);
-        isMoving = true;
     }
 
-    void stop()
+    void Stop()
     {
         Debug.Log("stop");
         rb.velocity = new Vector2(0f, 0f);
-        isMoving = false;
     }
 
     void Die()
@@ -88,6 +109,4 @@ public class EnnemyController : MonoBehaviour
         gameObject.SetActive(false);
         Destroy(gameObject, 1f);
     }
-
-
 }
