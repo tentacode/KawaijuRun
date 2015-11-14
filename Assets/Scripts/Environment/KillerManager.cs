36216da11e72c6e63a1d9be07745f33ebda3e687
leolefebvre <leolefebvre.contact@gmail.com>
2015-11-14 20:22:22 +0100

    Add killer trigger

---
+++ Assets/Scripts/Environment/KillerManager.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KillerManager : MonoBehaviour
+{
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.tag == "Ennemy")
+        {
+            Destroy(other.gameObject, 0.5f);
+        }
+    }
+}
