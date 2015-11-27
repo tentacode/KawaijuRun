7d346e9617f5db510183b74abc9ee741c1567d68
tentacode <contact@gabrielpillet.com>
2015-11-27 13:45:21 +0100

    Title and explosion sounds

---
+++ Assets/Scripts/FX/PlayRandomAudio.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PlayRandomAudio : MonoBehaviour
+{
+    public List<AudioClip> sounds;
+
+	void Start ()
+    {
+        AudioClip sound = sounds[Random.Range(0, sounds.Count)];
+        AudioSource source = GetComponent<AudioSource>();
+        source.clip = sound;
+        source.Play();
+	}
+}
